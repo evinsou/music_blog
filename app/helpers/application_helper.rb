bc185a738cb242f8dc73ad4a2dcf8f4d0ca903cf
evinsou <evinsou@gmail.com>
2013-02-19 13:48:16 +0200

    add page title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
+ # encoding: UTF-8
 module ApplicationHelper
+  def page_title
+    static_pages = {"announcement" => 'Анонс', 'about_myself' => 'О себе', 'billboard' => 'Афиша', 'photos' => 'Фотографии', 'video_records' => 'Видеозаписи', 'reviews' => 'Рецензии', 'side_notes' => 'Заметки на полях', 'links' => 'Ссылки'}
+    dinamic_pages = {'songs' => 'Репертуар', 'disks' => 'Дискография', 'feedbacks' => 'Гостевая', 'messages' => 'Контакты'}
+    static_pages[params[:id]] || dinamic_pages[params[:controller]]
+  end
 end
