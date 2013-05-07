0d90445e93921f6bd9c53468430dc5e03857cf5a
evinsou <evinsou@gmail.com>
2013-05-07 22:06:53 +0300

    deleted migrations

--- db/migrate/20130424145454_create_video_records.rb
+++
@@ -1,9 +0,0 @@
-class CreateVideoRecords < ActiveRecord::Migration
-  def change
-    create_table :video_records do |t|
-      t.string :title
-      t.string :youtube_url
-      t.timestamps
-    end
-  end
-end
